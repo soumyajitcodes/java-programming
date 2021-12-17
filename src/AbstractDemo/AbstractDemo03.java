2142597554e9a59d8d250f59b3904108e05521c4
Soumyajit <soumyajitdas400@gmail.com>
2021-12-17 17:06:21 +0530

    added files

---
+++ src/AbstractDemo/AbstractDemo03.java
@@ -0,0 +1,56 @@
+package AbstractDemo;
+
+abstract class Shape {
+    abstract void area();
+    abstract void perimeter();
+}
+
+class Rectangle extends Shape {
+    private int length;
+    private int breadth;
+
+    Rectangle (int l, int b) {
+        this.length = l;
+        this.breadth = b;
+    }
+
+    void perimeter() {
+        System.out.println("Perimeter of Rectangle : "+(2*(this.length+this.breadth))+" units.");
+    }
+
+    void area() {
+        System.out.println("Area of Rectangle      : "+(this.length*this.breadth)+" sq. units.");
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    Circle(double r) {
+        this.radius = r;
+    }
+
+    double circumference() {
+        return 2*Math.PI*this.radius;
+    }
+
+    void perimeter() {
+        System.out.format("Perimeter of Circle : %.02f units. \n", circumference());
+    }
+
+    void area() {
+        System.out.format("Area of Circle      : %.02f sq. units\n", Math.PI*Math.pow(this.radius,2));
+    }
+}
+
+public class AbstractDemo03 {
+    public static void main(String[] args) {
+        Shape s1 = new Rectangle(10, 20);
+        s1.perimeter();
+        s1.area();
+
+        Shape s2 = new Circle(10);
+        s2.perimeter();
+        s2.area();
+    }
+}
